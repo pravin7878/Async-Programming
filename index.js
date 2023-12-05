6132707171c6369426af70a2244f340e2bb416b4
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-12-06 00:47:12 +0530

    complet

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let second = 0;
-let minet = 0;
+// let minet = 0;
 let time;
 let display = document.getElementsByClassName("display")
 
 function start() {
-    time = setInterval(() => {
-        second++
-        if(second<10){
-            display[0].innerText = `${minet}:0${second}`
-        }
-       else{
-        display[0].innerText = `${minet}:${second}`;
-       }
-    }, 1000);
+    time = setInterval(stopwatch
+        // () => {
+    //     second++
+    //     if(second<10){
+    //         display[0].innerText = `${minet}:0${second}`
+    //     }
+    //    else{
+    //     display[0].innerText = `${minet}:${second}`;
+    //    }
+    // }
+    , 1000);
     console.log(time)
 }
 
@@ -26,3 +28,14 @@ function Reset() {
     display[0].innerText = second;
 }
 
+function stopwatch(){
+    let minet = Math.floor(second/60)
+    let repetsecond = Math.floor(second%60)
+    second++
+        if(repetsecond<10){
+            display[0].innerText = `0${minet}:0${repetsecond}`
+        }
+       else{
+        display[0].innerText = `0${minet}:${repetsecond}`;
+       }
+}
