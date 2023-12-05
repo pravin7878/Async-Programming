fdd60e95ad2f8de8bb8a5c4e0b38e84f3d3cac45
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-12-06 01:10:15 +0530

    js timer

---
+++ index.js
@@ -0,0 +1,33 @@
+let second = 0;
+let minet = 0;
+let hours = 0;
+let timer;
+
+let inp1 = document.getElementsByClassName('inp1')
+let inp2 = document.getElementsByClassName('inp2')
+let inp3 = document.getElementsByClassName('inp3')
+
+ second = inp3.value;
+ minet = inp2.value;
+ hours = inp1.value;
+
+
+
+// inp1[0].value = `00`
+// inp2[0].value = `00`
+// inp3[0].value = `00`
+
+function start() {
+    // setInterval({
+    //     hours--
+    // }, 1000);
+    console.log(inp3[0].value)
+}
+
+function stop() {
+
+}
+
+function reset() {
+
+}
