28431e91a821fcee59973cf77b2e0f67e328f615
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-12-08 23:47:41 +0530

    timer

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-let second = 0;
-let minet = 0;
-let hours = 0;
-let timer;
 
-let inp1 = document.getElementsByClassName('inp1')
-let inp2 = document.getElementsByClassName('inp2')
-let inp3 = document.getElementsByClassName('inp3')
-
- second = inp3.value;
- minet = inp2.value;
- hours = inp1.value;
-
-
-
-// inp1[0].value = `00`
-// inp2[0].value = `00`
-// inp3[0].value = `00`
+let inp1 = document.querySelector(".inp1")
+let inp2 = document.querySelector(".inp2")
+let inp3 = document.querySelector(".inp3")
 
 function start() {
-    // setInterval({
-    //     hours--
-    // }, 1000);
-    console.log(inp3[0].value)
+    setInterval(timer, 1000);
+    // console.log(inp3[0].value)
+    // console.log(hours)
 }
 
 function stop() {
@@ -29,5 +14,27 @@ function stop() {
 }
 
 function reset() {
+
+}
+function timer() {
+    let hours = inp1.value;
+    let minet = inp2.value;
+    let second = inp3.value;
+    // minet = Math.floor(second/60) 
+    // let repeatsecons = Math.floor(second%60) 
+    // second--
+    // minet == Math.floor(minet/60)
+    // inp3.value = second;
+    // inp2.value = minet;
+    // inp1.value = hours;
+
+    // for (let i = minet; i >= 0; i--) {
+    //     for (let j = 60; j >= 0; j--) {
+    //         // console.log(minet, j)
+    //         j = inp3.value
+
+    //     }
+    //     i = inp2.value
+    // }
 
 }
